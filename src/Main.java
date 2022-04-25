661daeb43d5ca5d78070af2c058336458372148d
LudmilaBatalova <www.lucybatalova@mail.ru>
2022-04-25 23:21:49 +0300

    Code Optimization

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int balance = 900;
         int payment = 1600;
-        int bonus = 0;
-        if (payment > 1000) {
-            bonus = payment / 100;
-            balance = balance + bonus + payment;
-        } else {
-            balance = balance + payment;
-        }
+
+        int bonus = payment > 1000 ? payment / 100 : 0;
+        balance +=  bonus + payment;
+
         System.out.println("Ваш баланс " + balance);
         System.out.println("Пополнен счет на " + payment);
         System.out.println("Начислено " + bonus + " бонусных рублей");
